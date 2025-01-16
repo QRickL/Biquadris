6db4d8f8ec87a224999dfac27ca19829efe6e6b0
QRickL <rickli1738@gmail.com>
2025-01-15 22:59:01 -0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Biquadris: by" << endl;
+  
+  cout << "David Gan" << endl;
+  cout << "Rick Li" << endl;
+  cout << "Molly Wu" << endl;
+  
+  cerr << "Code not publicly available due to the University of Waterloo's Policy 71 regulations" << endl;
+  return 0;
+}
