056a7d68136cacd8f706c75aaa12645f34e2df18
Rick Li <rickli1738@gmail.com>
2025-11-01 23:13:58 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,14 +1,14 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-  cout << "Biquadris: by" << endl;
   
-  cout << "David Gan" << endl;
-  cout << "Rick Li" << endl;
-  cout << "Molly Wu" << endl;
+  std::cout << "Biquadris: by" << std::endl;
   
-  cerr << "Code not publicly available due to the University of Waterloo's Policy 71 regulations" << endl;
-  return 0;
+  std::cout << "David Gan"     << std::endl;
+  std::cout << "Rick Li"       << std::endl;
+  std::cout << "Molly Wu"      << std::endl;
+  
+  std::cerr << "Code not publicly available due to the University of Waterloo's Policy 71 regulations" << std::endl;
+  return 1;
+  
 }
